a423aa6ffa9a81253f0c9b46a7778ae1da3d2623
Cleiton Rodrigues <rdgsdev@gmail.com>
2022-07-19 14:54:32 -0300

    replace old references to valora

--- cmd.sh
+++ cmd.sh
@@ -6,19 +6,19 @@ echo "Restarting memcached"
 
 if [[ $COMMAND = "celeryworker" ]]; then
     echo "Running Celery Worker"
-    exec celery worker -E -A valora --loglevel=info
+    exec celery worker -E -A bus --loglevel=info
 
 elif [[ $COMMAND = "celerypriorityworker" ]]; then
     echo "Running Celery Priority Worker"
-    exec celery worker -E -A valora -n priority -Q priority --loglevel=info --pool=solo
+    exec celery worker -E -A bus -n priority -Q priority --loglevel=info --pool=solo
 
 elif [[ $COMMAND = "celerybeat" ]]; then
     echo "Running Celery Beat"
-    exec celery beat -A valora --loglevel=debug --pidfile=/tmp/celerybeat.pid --scheduler django_celery_beat.schedulers:DatabaseScheduler
+    exec celery beat -A bus --loglevel=debug --pidfile=/tmp/celerybeat.pid --scheduler django_celery_beat.schedulers:DatabaseScheduler
 
 elif [[ $COMMAND = "celeryflower" ]]; then
     echo "Running Celery Flower"
-    exec celery flower -A valora --basic_auth=adminvalora:flowervaloraadm1 --loglevel=info
+    exec celery flower -A bus --basic_auth=adminbus:flowerbusadm1 --loglevel=info
 
 elif [[ $COMMAND = "uwsgi" ]]; then
     echo "Running uWSGI Server"
@@ -26,11 +26,11 @@ elif [[ $COMMAND = "uwsgi" ]]; then
 
 elif [[ $COMMAND = "daphne" ]]; then
     echo "Running with daphne"
-    exec daphne -b 0.0.0.0 -p 80 valora.asgi:application
+    exec daphne -b 0.0.0.0 -p 80 bus.asgi:application
 
 elif [[ $COMMAND = "gunicorn" ]]; then
     echo "Running with gunicorn"
-    exec gunicorn valora.asgi:application -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 --threads 3 --reload --access-logfile access.log --error-logfile error.log --log-level error --keep-alive 3 --max-requests 5000 --worker-connections=1000
+    exec gunicorn bus.asgi:application -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:80 --threads 3 --reload --access-logfile access.log --error-logfile error.log --log-level error --keep-alive 3 --max-requests 5000 --worker-connections=1000
 
 elif [[ $COMMAND = "test" ]]; then
     echo "Running tests"
